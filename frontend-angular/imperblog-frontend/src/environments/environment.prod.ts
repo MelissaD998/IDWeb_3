c34783eac4f079683852be22c16e9c5d43731b47
melis <melissa.dragutan@yahoo.com>
2022-12-11 21:08:19 +0200

    configure project to run with docker

--- frontend-angular/imperblog-frontend/src/environments/environment.prod.ts
+++ frontend-angular/imperblog-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+    baseUrl: 'http://localhost:8080',
+    tokenAllowedDomains: ["localhost:8080"],
+    disallowedRoutes: [/\/oauth\/token/, /\/api\/auth\/signup/]
 };
